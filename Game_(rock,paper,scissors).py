349eb4f7c13af5a3cbbc800237ea1b4ff2351d01
VanshCoder7 <limbasiyavansh70@gmail.com>
2025-05-23 12:47:50 +0530

    Add files via upload

---
+++ Game_(rock,paper,scissors).py
@@ -0,0 +1,199 @@
+
+print('\n')
+Players = input("Hello,\nHow many player want to play (rock,paper,scissors) game? = ")
+if (int(Players)<=0):
+    print("Invaild import!")
+
+elif (int(Players)==1):
+    print("This game can't play solo!")
+
+elif (int(Players)==2):
+    print('Ok!, This game can play two players.')
+    Ready = input("Are you ready to play (rock,paper,scissors) game? (Yes/No) = ")
+    if Ready == 'Yes' or 'yes':
+        p1 = input("Enter player 1 name : ")
+        p2 = input("Enter player 2 name : ")
+        p1 = ' '.join(p1.split())
+        if p1.replace(" ","").isalpha():
+            p2 = ' '.join(p1.split())
+            if p2.replace(" ", "").isalpha(): 
+                print('_____(GAME START!)_____')
+                p1_str = input(f'Enter {p1} your choice (rock,paper,scissors)? = ')
+                p1_str = ' '.join(p1_str.split())
+                if p1_str.replace(" ","").isalpha():
+                    p2_str = input(f'Enter {p2} your choice (rock,paper,scissors)? = ')
+                    p2_str = ' '.join(p2_str.split())
+                    if p2_str.replace(" ","").isalpha():
+                        p1_set = {"rock":0, "paper":1, "scissors":2}
+                        p1_revesedset = {0:"rock", 1:"paper", 2:"scissors"}
+                        p2_set = {"rock":0, "paper":1, "scissors":2}
+                        p2_revesedset = {0:"rock", 1:"paper", 2:"scissors"}
+
+                        you1 = p1_set[p1_str]
+                        you2 = p2_set[p2_str]
+
+                        print('')
+                        print(f"{p1} your choive is : {p1_revesedset[you1]}")
+                        print(f"{p2} your choive is : {p2_revesedset[you2]}")
+                        print('')
+
+                        if (you1 == you2):
+                            print("It's drow!")
+
+                        elif (you1 == 0 and you2 == 1):
+                            print(f"{p2} is Win!")
+
+                        elif (you1 == 0 and you2 == 2):
+                            print(f"{p1} is Win!")
+
+                        elif (you1 == 1 and you2 == 0):
+                            print(f"{p1} is Win!")
+
+                        elif (you1 == 1 and you2 == 2):
+                            print(f"{p2} is Win!")
+
+                        elif (you1 == 2 and you2 == 0):
+                            print(f"{p2} is Win!")
+
+                        elif (you1 == 2 and you2 == 1):
+                            print(f"{p1} is Win!")
+
+                    else:
+                        print(f"{p2_str} is not vaild input!(rock,paper,scissors?)")
+
+                else:
+                    print(f"{p1_str} is not vaild input!(rock,paper,scissors)?")                
+            
+            else:
+                print(f"Enter {p2} vaild input!")
+        
+        else:
+            print(f"Enter {p1} vaild input!")
+
+    else:
+        print("Ok!, Game closing...")
+
+elif (int(Players)==3):
+    print("Ok!, This game play 3 players. ")
+    Ready2 = input("Are you ready to play (rock,paper,scissors) game? (Yes/No)  = ")
+    if Ready2 == 'Yes':
+        p1 = input("Enter player 1 name : ")
+        p2 = input("Enter player 2 name : ")
+        p3 = input("Enter player 3 name : ")
+        p1 = ' '.join(p1.split())
+        if p1.replace(" ", "").isalpha():
+            p2 = ' '.join(p1.split())
+            if p2.replace(" ", "").isalpha():
+                p3 = ' '.join(p1.split())
+                if p3.replace(" ", "").isalpha():
+                    print('_____(GAME START!)_____')
+                    p1_str = input(f'Enter {p1} your choice (rock,paper,scissors)? = ')
+                    p1_str = ' '.join(p1.split())
+                    if p1_str.replace(" ", "").isalpha():
+                        p2_str = input(f'Enter {p2} your choice (rock,paper,scissors)? = ')
+                        p2_str = ' '.join(p1_str.split())
+                        if p1_str.replace(" ", "").isalpha():
+                            p3_str = input(f'Enter {p3} your choice (rock,paper,scissors)? = ')
+                            p3_str = ' '.join(p3_str.split())
+                            if p3_str.replace(" ", "").isalpha():
+                                p1_set = {"rock":0, "paper":1, "scissors":2}
+                                p1_revesedset = {0:"rock", 1:"paper", 2:"scissors"}
+                                p2_set = {"rock":0, "paper":1, "scissors":2}
+                                p2_revesedset = {0:"rock", 1:"paper", 2:"scissors"}
+                                p3_set = {"rock":0, "paper":1, "scissors":2}
+                                p3_revesedset = {0:"rock", 1:"paper", 2:"scissors"}
+
+                                you1 = p1_set[p1_str]
+                                you2 = p2_set[p2_str]    
+                                you3 = p3_set[p3_str]
+
+                                print('')    
+                                print(f"{p1} your choive is : {p1_revesedset[you1]}")
+                                print(f"{p2} your choive is : {p2_revesedset[you2]}")
+                                print(f"{p3} your choive is : {p3_revesedset[you3]}")
+                                print('')    
+
+                                if (you1 == you2 == you3):
+                                    print("I's drow!")
+            # 0
+                                elif (you1 == 0 and you2 == 1 and you3 == 2):
+                                    print(f"Noo any Win!")
+                            
+                                elif (you1 == 0 and you2 == 2 and you3 == 1):
+                                    print(f"Noo any Win!")
+                                
+                                elif (you1 == 0 and you2 == 1 and you3 == 1):
+                                    print(f"{p2} and {p3} is Win!")
+                            
+                                elif (you1 == 0 and you2 == 2 and you3 == 2):
+                                    print(f"{p1} is Win!")
+
+                                elif (you1 == 0 and you2 == 1 and you3 == 0):
+                                    print(f"{p2} is Win!")
+
+                                elif (you1 == 0 and you2 == 0 and you3 == 1):
+                                    print(f"{p3} is Win!")
+
+                                elif (you1 == 0 and you2 == 0 and you3 == 2):
+                                    print(f"{p1} and {p2} is Win!")        
+            # 1
+                                elif (you1 == 1 and you2 == 2 and you3 == 0):
+                                    print(f"Noo any Win!")
+                            
+                                elif (you1 == 1 and you2 == 0 and you3 == 2):
+                                    print(f"Noo any Win!")
+                                
+                                elif (you1 == 1 and you2 == 0 and you3 == 0):
+                                    print(f"{p1} is Win!")
+                            
+                                elif (you1 == 1 and you2 == 2 and you3 == 2):
+                                    print(f"{p2} and {p3} is Win!")
+
+                                elif (you1 == 1 and you2 == 0 and you3 == 1):
+                                    print(f"{p1} and  {p3} is Win!")
+
+                                elif (you1 == 1 and you2 == 1 and you3 == 0):
+                                    print(f"{p1} and {p2} is Win!")
+
+                                elif (you1 == 1 and you2 == 1 and you3 == 2):
+                                    print(f"{p3} is Win!")            
+            # 2
+                                elif (you1 == 2 and you2 == 1 and you3 == 0):
+                                    print(f"Noo any Win!")
+                            
+                                elif (you1 == 2 and you2 == 0 and you3 == 1):
+                                    print(f"Noo any Win!")
+                                
+                                elif (you1 == 2 and you2 == 0 and you3 == 0):
+                                    print(f"{p2} and {p3} is Win!")
+                            
+                                elif (you1 == 2 and you2 == 1 and you3 == 1):
+                                    print(f"{p1} is Win!")
+
+                                elif (you1 == 2 and you2 == 0 and you3 == 2):
+                                    print(f"{p2} is Win!")
+
+                                elif (you1 == 2 and you2 == 2 and you3 == 0):
+                                    print(f"{p3} is Win!")
+
+                                elif (you1 == 2 and you2 == 2 and you3 == 1):
+                                    print(f"{p1} and {p2} is Win!")
+
+                            else:
+                                print(f"{p3_str} is not vaild input!(son,paper,scissors)")        
+
+                        else:
+                            print(f"{p2_str} is not vaild input!(son,paper,scissors)")        
+
+                    else:
+                        print(f"{p1_str} is not vaild input!(son,paper,scissors)")
+
+                else:
+                    print(f"Enter {p3} vaild input!")
+            else:
+                print(f"Enter {p2} vaild input!")
+        else:
+            print(f"Enter {p1} vaild input!")
+    
+    else:
+        print("Ok!, Game closing...")    
